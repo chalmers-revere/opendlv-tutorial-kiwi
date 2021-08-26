e1d691a93a03097ca0dd9e863c9ee4d67caa642a
Anton Kullberg <anton.kullberg@liu.se>
2021-08-26 12:10:40 +0200

    feat: added control microservice

---
+++ kiwi-control/src/behavior.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2018 Ola Benderius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "behavior.hpp"
+
+Behavior::Behavior() noexcept:
+  m_frontUltrasonicReading{},
+  m_rearUltrasonicReading{},
+  m_leftIrReading{},
+  m_rightIrReading{},
+  m_groundSteeringAngleRequest{},
+  m_pedalPositionRequest{},
+  m_frontUltrasonicReadingMutex{},
+  m_rearUltrasonicReadingMutex{},
+  m_leftIrReadingMutex{},
+  m_rightIrReadingMutex{},
+  m_groundSteeringAngleRequestMutex{},
+  m_pedalPositionRequestMutex{}
+{
+}
+
+opendlv::proxy::GroundSteeringRequest Behavior::getGroundSteeringAngle() noexcept
+{
+  std::lock_guard<std::mutex> lock(m_groundSteeringAngleRequestMutex);
+  return m_groundSteeringAngleRequest;
+}
+
+opendlv::proxy::PedalPositionRequest Behavior::getPedalPositionRequest() noexcept
+{
+  std::lock_guard<std::mutex> lock(m_pedalPositionRequestMutex);
+  return m_pedalPositionRequest;
+}
+
+void Behavior::setFrontUltrasonic(opendlv::proxy::DistanceReading const &frontUltrasonicReading) noexcept
+{
+  std::lock_guard<std::mutex> lock(m_frontUltrasonicReadingMutex);
+  m_frontUltrasonicReading = frontUltrasonicReading;
+}
+
+void Behavior::setRearUltrasonic(opendlv::proxy::DistanceReading const &rearUltrasonicReading) noexcept
+{
+  std::lock_guard<std::mutex> lock(m_rearUltrasonicReadingMutex);
+  m_rearUltrasonicReading = rearUltrasonicReading;
+}
+
+void Behavior::setLeftIr(opendlv::proxy::VoltageReading const &leftIrReading) noexcept
+{
+  std::lock_guard<std::mutex> lock(m_leftIrReadingMutex);
+  m_leftIrReading = leftIrReading;
+}
+
+void Behavior::setRightIr(opendlv::proxy::VoltageReading const &rightIrReading) noexcept
+{
+  std::lock_guard<std::mutex> lock(m_rightIrReadingMutex);
+  m_rightIrReading = rightIrReading;
+}
+
+
+void Behavior::step() noexcept
+{
+  opendlv::proxy::DistanceReading frontUltrasonicReading;
+  opendlv::proxy::DistanceReading rearUltrasonicReading;
+  opendlv::proxy::VoltageReading leftIrReading;
+  opendlv::proxy::VoltageReading rightIrReading;
+  {
+    std::lock_guard<std::mutex> lock1(m_frontUltrasonicReadingMutex);
+    std::lock_guard<std::mutex> lock2(m_rearUltrasonicReadingMutex);
+    std::lock_guard<std::mutex> lock3(m_leftIrReadingMutex);
+    std::lock_guard<std::mutex> lock4(m_rightIrReadingMutex);
+
+    frontUltrasonicReading = m_frontUltrasonicReading;
+    rearUltrasonicReading = m_rearUltrasonicReading;
+    leftIrReading = m_leftIrReading;
+    rightIrReading = m_rightIrReading;
+  }
+
+  float frontDistance = frontUltrasonicReading.distance();
+  float rearDistance = rearUltrasonicReading.distance();
+  double leftDistance = convertIrVoltageToDistance(leftIrReading.voltage());
+  double rightDistance = convertIrVoltageToDistance(rightIrReading.voltage());
+
+  float pedalPosition = 0.2f;
+  float groundSteeringAngle = 0.3f;
+  if (frontDistance < 0.3f) {
+    pedalPosition = 0.0f;
+  } else {
+    if (rearDistance < 0.3f) {
+      pedalPosition = 0.4f;
+    }
+  }
+
+  if (leftDistance < rightDistance) {
+    if (leftDistance < 0.2f) {
+      groundSteeringAngle = 0.2f;
+    }
+  } else {
+    if (rightDistance < 0.2f) {
+      groundSteeringAngle = 0.2f;
+    }
+  }
+
+  {
+    std::lock_guard<std::mutex> lock1(m_groundSteeringAngleRequestMutex);
+    std::lock_guard<std::mutex> lock2(m_pedalPositionRequestMutex);
+
+    opendlv::proxy::GroundSteeringRequest groundSteeringAngleRequest;
+    groundSteeringAngleRequest.groundSteering(groundSteeringAngle);
+    m_groundSteeringAngleRequest = groundSteeringAngleRequest;
+
+    opendlv::proxy::PedalPositionRequest pedalPositionRequest;
+    pedalPositionRequest.position(pedalPosition);
+    m_pedalPositionRequest = pedalPositionRequest;
+  }
+}
+
+// TODO: This is a rough estimate, improve by looking into the sensor specifications.
+double Behavior::convertIrVoltageToDistance(float voltage) const noexcept
+{
+  double voltageDividerR1 = 1000.0;
+  double voltageDividerR2 = 1000.0;
+
+  double sensorVoltage = (voltageDividerR1 + voltageDividerR2) / voltageDividerR2 * voltage;
+  double distance = (2.5 - sensorVoltage) / 0.07;
+  return distance;
+}
---
+++ kiwi-control/src/opendlv-logic-test-kiwi.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2018 Ola Benderius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
+#include "behavior.hpp"
+
+int32_t main(int32_t argc, char **argv) {
+  int32_t retCode{0};
+  auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+  if (0 == commandlineArguments.count("cid") || 0 == commandlineArguments.count("freq")) {
+    std::cerr << argv[0] << " tests the Kiwi platform by sending actuation commands and reacting to sensor input." << std::endl;
+    std::cerr << "Usage:   " << argv[0] << " --freq=<Integration frequency> --cid=<OpenDaVINCI session> [--verbose]" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --freq=10 --cid=111" << std::endl;
+    retCode = 1;
+  } else {
+    bool const VERBOSE{commandlineArguments.count("verbose") != 0};
+    uint16_t const CID = std::stoi(commandlineArguments["cid"]);
+    float const FREQ = std::stof(commandlineArguments["freq"]);
+
+    Behavior behavior;
+
+    auto onDistanceReading{[&behavior](cluon::data::Envelope &&envelope)
+      {
+        auto distanceReading = cluon::extractMessage<opendlv::proxy::DistanceReading>(std::move(envelope));
+        uint32_t const senderStamp = envelope.senderStamp();
+        if (senderStamp == 0) {
+          behavior.setFrontUltrasonic(distanceReading);
+        } else if (senderStamp == 1) {
+          behavior.setRearUltrasonic(distanceReading);
+        }
+      }};
+    auto onVoltageReading{[&behavior](cluon::data::Envelope &&envelope)
+      {
+        auto voltageReading = cluon::extractMessage<opendlv::proxy::VoltageReading>(std::move(envelope));
+        uint32_t const senderStamp = envelope.senderStamp();
+        if (senderStamp == 0) {
+          behavior.setLeftIr(voltageReading);
+        } else if (senderStamp == 1) {
+          behavior.setRightIr(voltageReading);
+        }
+      }};
+
+    cluon::OD4Session od4{CID};
+    od4.dataTrigger(opendlv::proxy::DistanceReading::ID(), onDistanceReading);
+    od4.dataTrigger(opendlv::proxy::VoltageReading::ID(), onVoltageReading);
+
+    auto atFrequency{[&VERBOSE, &behavior, &od4]() -> bool
+      {
+        behavior.step();
+        auto groundSteeringAngleRequest = behavior.getGroundSteeringAngle();
+        auto pedalPositionRequest = behavior.getPedalPositionRequest();
+
+        cluon::data::TimeStamp sampleTime = cluon::time::now();
+        od4.send(groundSteeringAngleRequest, sampleTime, 0);
+        od4.send(pedalPositionRequest, sampleTime, 0);
+        if (VERBOSE) {
+          std::cout << "Ground steering angle is " << groundSteeringAngleRequest.groundSteering()
+            << " and pedal position is " << pedalPositionRequest.position() << std::endl;
+        }
+
+        return true;
+      }};
+
+    od4.timeTrigger(FREQ, atFrequency);
+  }
+  return retCode;
+}
